dd645ae51addaa85941958d2d74d90a0bd5da787
ericyangchen <yericchenyang@gmail.com>
2025-09-30 03:38:00 -0400

    feat: sorting

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,14 @@ class Movie < ActiveRecord::Base
   end
 
   def self.with_ratings(ratings)
-    where(rating: ratings)
+    return where(rating: ratings)
+  end
+
+  def self.sorted_by(sort_by)
+    if sort_by.present?
+      return order(sort_by)
+    else
+      return all
+    end
   end
 end
